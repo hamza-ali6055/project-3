aabd2b839e3af26cdd12c9ad327e22ddf64e15dd
hamza-ali6055 <hamzaalig6055@gmail.com>
2024-10-24 15:01:31 +0500

    Next.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,27 @@
+import { PluginAPI } from "tailwindcss/types/config";
 import type { Config } from "tailwindcss";
+import { sub } from "framer-motion/client";
+
+function addVariableForColors({addBase,theme}:PluginAPI) {
+  const allColors = theme('colors',{}) ||{}
+  const newWars: Record<string, string> = {};
+  Object.entries(allColors).forEach(([key,value]) => {
+   if(typeof value === 'string'){
+    newWars['--${key}']= value;
+   }
+    else if(typeof value ==='object' && value !== null){
+      Object.entries(value).forEach(([subKey, subValue]) =>{
+        if(typeof subValue === "string"){
+          newWars[`--${key}-${subKey}`]= subValue;
+        }
+      });
+    }
+  });
+}
+
+
+
+
 
 const config: Config = {
   content: [
@@ -11,6 +34,22 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      
+      },
+      animation: {
+        spotlight : "spotlight 2s ease .75 1 forwards",
+      },
+      keyframes : {
+        spotlight : {
+          "0%" : {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)"
+          },
+          "100" : {
+            opacity: "1",
+            transform: "translate(-50%, -40%) scale(1)"
+          },
+        },
       },
     },
   },
